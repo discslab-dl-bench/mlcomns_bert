6ff2c8dfa174d2b1c623aaa1df227823647cc3c0
Shamauk <zacharydoucet01@hotmail.com>
2022-08-15 16:21:04 -0400

    Enabled adding output directory as param to start_training.sh

--- start_training.sh
+++ start_training.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-#TODO: Parametrize num_gpus and mount points
-docker run -it --rm --gpus all -v /raid/data/bert/wiki:/wiki -v /raid/data/bert/raw-data:/raw-data -v /raid/data/bert/preproc_data:/data -v /raid/data/bert/output:/output tf-bert:latest /bin/bash train_model.sh
+OUTPUTDIR='/raid/data/bert/output'
+
+if [ -z $1 ]
+then
+	echo "Saving to default output directory"
+else
+	if [ -d $1 ] 
+	then
+		echo "Setting directory path to ${1}"
+		OUTPUTDIR=$1
+	else
+		echo 'You must provide a valid directory path'
+		exit 1
+	fi
+fi	
+
+
+docker run -it --rm --gpus all -v /raid/data/bert/wiki:/wiki -v /raid/data/bert/raw-data:/raw-data -v /raid/data/bert/preproc_data:/data -v $OUTPUTDIR:/output tf-bert:latest /bin/bash train_model.sh
