05978649940e49a7c0d84d17c74db62175fc58ae
Shamauk <zacharydoucet01@hotmail.com>
2022-08-17 09:18:29 -0400

    Allowed for memory restriction

--- start_training.sh
+++ start_training.sh
@@ -5,17 +5,19 @@ MAX_NUM_GPUS=8
 
 # Defaults 
 OUTPUT_DIR='/raid/data/bert/output'
-DATA_DIR='/raid/data/bert/preproc_data'
+DATA_DIR='/raid/data/bert/preproc_data/original'
 NUM_GPUS=8
+AMT_MEMORY=-1
 
 # Getting each named parameter
-while getopts g:o:d:h flag
+while getopts g:o:d:m:h flag
 do
 	case "${flag}" in
 		g) NUM_GPUS=${OPTARG};;
 		o) OUTPUT_DIR=${OPTARG};;
 		d) DATA_DIR=${OPTARG};;
-		h) echo "-g: number of gpus"; echo "-o: checkpoint output dir"; echo "-d: TFRecord directory"; echo "-h: this page"; exit 1;
+		m) AMT_MEMORY=${OPTARG};;
+		h) echo "-g: number of gpus"; echo "-o: checkpoint output dir"; echo "-d: TFRecord directory"; echo "-m: memory limit in GB"; echo "-h: this page"; exit 1;
 	esac
 done
 
@@ -38,9 +40,15 @@ then
 		exit -4
 fi
 
-echo $OUTPUT_DIR
-echo $NUM_GPUS
-echo $DATA_DIR
+# Building up the command
+COMMAND="docker run -it --rm --gpus all -v /raid/data/bert/wiki:/wiki -v /raid/data/bert/raw-data:/raw-data -v ${DATA_DIR}:/data -v ${OUTPUT_DIR}:/output" 
 
 
-#docker run -it --rm --gpus all -v /raid/data/bert/wiki:/wiki -v /raid/data/bert/raw-data:/raw-data -v DATA_DIR:/data -v $OUTPUT_DIR:/output tf-bert:latest /bin/bash train_model.sh $NUM_GPUS
+if [ $AMT_MEMORY -gt 0 ]
+then
+		COMMAND="${COMMAND} -m ${AMT_MEMORY}g"
+fi
+
+COMMAND="${COMMAND} tf-bert:latest /bin/bash train_model.sh ${NUM_GPUS}"
+
+exec $COMMAND
