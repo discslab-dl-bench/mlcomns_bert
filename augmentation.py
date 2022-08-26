f55e76654eb85af24872bda27d1dc1a6fa4746ac
Yuyan-C <yuyan.chen2@mail.mcgill.ca>
2022-08-26 09:37:32 -0400

    fix: add newline character

--- augmentation.py
+++ augmentation.py
@@ -1,31 +1,7 @@
 import random
 import nltk
 from nltk.corpus import wordnet
-import absl
 import argparse
-
-# flags = absl.flags
-# FLAGS = flags.FLAGS
-
-# flags.DEFINE_string(
-#     "input", None,
-#     "The file to be augmented."
-# )
-
-# flags.DEFINE_string(
-#     "output", None,
-#     "The file to save the augmented text."
-# )
-
-# flags.DEFINE_string(
-#     "method", None,
-#     "The augmentation method."
-# )
-
-# flags.DEFINE_float(
-#     'number', None, 'Number of times (RI, RS, SR) or probability (RD).'
-# )
-
 
 def get_synonym(word):
     """
@@ -50,10 +26,14 @@ def random_swap(text: str, number: int) -> str:
     wc = len(words)
     if wc < 2:
         return text
+    words[-1] = words[-1][:len(words[-1])-1] # get rid of the newline character
     for i in range(number):
         pos1, pos2 = random.sample(range(wc), 2)
         words[pos1], words[pos2] = words[pos2], words[pos1]
-    return ' '.join(words)
+    return ' '.join(words) + '\n'
+
+
+
 
 
 def random_insertion(text, number):
@@ -67,6 +47,7 @@ def random_insertion(text, number):
     :return:
     """
     words = text.split(' ')
+    words[-1] = words[-1][:len(words[-1])-1] # get rid of the newline character
     for i in range(number):
         wc = len(words)
         p = random.sample(range(wc), 1)[0]  # get the position of a random word
@@ -76,8 +57,7 @@ def random_insertion(text, number):
             syn_pos = random.sample(range(swc), 1)[0]  # get a random synonym of the random word
             to_insert = random.sample(range(wc), 1)[0]  # get a random position to insert the synonym
             words.insert(to_insert, synwords[syn_pos])
-
-    return ' '.join(words)
+    return ' '.join(words) + '\n'
 
 
 def random_deletion(text: str, number) -> str:
@@ -89,7 +69,7 @@ def random_deletion(text: str, number) -> str:
     :return:
     """
     words = text.split(' ')
-
+    words[-1] = words[-1][:len(words[-1])-1]
     new_words = []
 
     for w in words:
@@ -99,33 +79,7 @@ def random_deletion(text: str, number) -> str:
     if len(new_words) < 1:
         pos = random.sample(range(len(words)), 1)[0]
         new_words.append(words[pos])
-
-    return ' '.join(new_words)
-
-
-def synonym_replacement(text: str, number: int) -> str:
-    """
-    (SR)
-    Randomly choose n words from the sentence.
-    Replace each of these words with one of its synonyms chosen at random.
-    :param text:
-    :param number: randomly replace n words with synonym
-    :return:
-    """
-    words = text.split(' ')
-    wc = len(words)
-    if wc < 1:
-        return text
-    indices = list(range(wc))
-    pos = random.sample(indices, number)
-
-    for p in pos:
-        synwords = get_synonym(words[p])
-        swc = len(synwords)
-        if swc > 1:
-            syn_pos = random.sample(range(swc), 1)[0]
-            words[p] = synwords[syn_pos]
-    return ' '.join(words)
+    return ' '.join(new_words) + '\n'
 
 
 def synonym_replacement(text: str, n: int) -> str:
@@ -137,6 +91,7 @@ def synonym_replacement(text: str, n: int) -> str:
     :return: a sentence
     """
     words = text.split(' ')
+    words[-1] = words[-1][:len(words[-1])-1]
     wc = len(words)
     if wc < n:
         return text
@@ -146,17 +101,17 @@ def synonym_replacement(text: str, n: int) -> str:
     for p in pos:
         synwords = get_synonym(words[p])
         swc = len(synwords)
-        if swc > 1:
+        if swc > 1:py
             syn_pos = random.sample(range(swc), 1)[0]
             words[p] = synwords[syn_pos]
-    return ' '.join(words)
+    return ' '.join(words) + '\n'
 
 
 FUNCTIONS = {
 "random_swap": random_swap,
 "random_insertion": random_insertion,
 "random_deletion": random_deletion,
-"synonym_replacemnet": synonym_replacement
+"synonym_replacement": synonym_replacement
 }
 
 def text_augmentation(input, output, method, number):
@@ -182,8 +137,6 @@ def text_augmentation(input, output, method, number):
             break
         func = FUNCTIONS[method] # the augmentation function
         new_text = func(text, number)
-        if "\n" not in new_text:  # make sure there is a newline character at the end of each sentence
-            new_text += '\n'
         f_new.write(new_text)
 
     f_original.close()
@@ -197,37 +150,9 @@ if __name__ == "__main__":
     PARSER = argparse.ArgumentParser()
     PARSER.add_argument('--input', dest='input', required=True)
     PARSER.add_argument('--output', dest='output', required=True)
-    PARSER.add_argument('--method', dest='method', choices=["random_swap", "random_insertion", "random_deletion", "synonym_replacemnet"])
+    PARSER.add_argument('--method', dest='method', choices=["random_swap", "random_insertion", "random_deletion", "synonym_replacement"])
     PARSER.add_argument('--number', dest='number')
     args = PARSER.parse_args()
 
     text_augmentation(args.input, args.output, args.method, args.number)
 
-
-    # flags.mark_flag_as_required("input")
-    # flags.mark_flag_as_required("output")
-    # flags.mark_flag_as_required("method")
-    # flags.mark_flag_as_required("number")
-    # absl.app.run(main)
-
-
-
-    # for i in range(10):
-    #     text = f_original.readline()
-    #     new_text = synonym_replacement(text, 5)
-    #     f_new.write(new_text)
-
-    # f_original.close()
-    # f_new.close()
-
-    # # while f_original:
-    # #     line = f_original.readline()
-    # #     print(line)
-    # #     if line == "":
-    # #         break
-
-    # # f_original.close()
-
-    # text = "The quick brown fox jumps over the lazy dog"
-    #
-    # print(new_text)
